a647f34676047a9bc97b71b103496e6a179f49b5
TextualMold9830 <88786053+TextualMold9830@users.noreply.github.com>
2024-09-04 22:54:43 +0300

    Server no longer loads all sounds when launched

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/ShatteredPixelDungeon.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/ShatteredPixelDungeon.java
@@ -111,7 +111,6 @@ public class ShatteredPixelDungeon extends Game {
 		Music.INSTANCE.enable( SPDSettings.music() );
 		Music.INSTANCE.volume( SPDSettings.musicVol()*SPDSettings.musicVol()/100f );
 
-		Sample.INSTANCE.load( Assets.Sounds.all );
 		Server.startServer();
 		Dungeon.init();
 		InterLevelSceneServer.mode = InterLevelSceneServer.Mode.DESCEND;
