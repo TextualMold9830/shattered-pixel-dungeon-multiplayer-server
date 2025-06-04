9b8566c642a19924819fad4e7e7c9417b01a6dd0
TextualMold9830 <88786053+TextualMold9830@users.noreply.github.com>
2025-06-04 22:02:44 +0300

    Bug fixes and more uses of binding

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/Dungeon.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/Dungeon.java
@@ -707,7 +707,6 @@ public class Dungeon {
 					saveHero(hero);
 				}
 			}
-			bundle.put(BALANCE_DATA, balance);
 		} catch (IOException e) {
 			GamesInProgress.setUnknown( save );
 			ShatteredPixelDungeon.reportException(e);
@@ -842,11 +841,6 @@ public class Dungeon {
 
 		Statistics.restoreFromBundle( bundle );
 		Generator.restoreFromBundle( bundle );
-		if (bundle.contains(BALANCE_DATA)) {
-			balance = (BalanceData) bundle.get(BALANCE_DATA);
-		} else {
-			balance = BalanceData.load();
-		}
 	}
 	
 	public static Level loadLevel() throws IOException {
