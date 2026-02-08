2a29f4d6caea33e2bea7548af36c66d3179c9455
TextualMold9830 <88786053+TextualMold9830@users.noreply.github.com>
2026-02-08 15:14:35 +0200

    Fixed MagesStaff not gaining more charges on fragment upgrade

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/MagesStaff.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/MagesStaff.java
@@ -325,8 +325,15 @@ public class MagesStaff extends MeleeWeapon {
 
 		return this;
 	}
-	
-	public void updateWand(boolean levelled){
+
+    @Override
+    public Item upgradeFragmented(Hero hero) {
+        super.upgradeFragmented(hero);
+        updateWand(true);
+        return this;
+    }
+
+    public void updateWand(boolean levelled){
 		if (wand != null) {
 			int curCharges = wand.getCurCharges();
 			wand.level(level(findOwner()));
@@ -334,6 +341,7 @@ public class MagesStaff extends MeleeWeapon {
 			wand.maxCharges = Math.min(wand.maxCharges + 1, 10);
 			wand.setCurCharges(Math.min(curCharges + (levelled ? 1 : 0), wand.maxCharges));
 			updateQuickslot();
+            sendSelfUpdate();
 		}
 	}
 
