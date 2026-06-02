d33022068bdbdbd8c6546a77bae7677fef8aac0f
Nikita22007 <game.nikita22007@yandex.ru>
2026-06-03 00:01:43 +0300

    Replace packAndAddLevelTiles with SetLevelTilesAction

---
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/network/actions/SetLevelTilesAction.java
@@ -0,0 +1,17 @@
+package com.shatteredpixel.shatteredpixeldungeon.network.actions;
+
+import com.shatteredpixel.shatteredpixeldungeon.levels.Level;
+import org.jetbrains.annotations.NotNull;
+
+public class SetLevelTilesAction implements NetworkAction {
+    public final Level level;
+
+    public SetLevelTilesAction(@NotNull Level level) {
+        this.level = level;
+    }
+
+    @Override
+    public @NotNull String actionName() {
+        return "set_level_tiles";
+    }
+}
