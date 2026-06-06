b4b4fd52a4beb4c1847ebf8deb9b055e46075420
Nikita22007 <game.nikita22007@yandex.ru>
2026-06-06 19:58:55 +0300

    return log code back

--- core/src/main/java/com/nikita22007/multiplayer/utils/Log.java
+++ core/src/main/java/com/nikita22007/multiplayer/utils/Log.java
@@ -6,19 +6,25 @@ import java.util.Date;
 public class Log {
 
     public static void w(String s, Object... params) {
+        print("W", s, params);
     }
 
     public static void wtf(String s, Object... params) {
+        print("WTF", s, params);
     }
 
     public static void e(String s, Object... params) {
+        print("E", s, params);
     }
 
     public static void i(String s, Object... params) {
+        print("I", s, params);
     }
 
     private static void print(String logLevel, String s, Object... params)
     {
-        System.out.println(String.format(( new SimpleDateFormat("HH:MM:SS").format(new Date())), logLevel, String.format(s,params)));
+        String time = new SimpleDateFormat("HH:mm:ss").format(new Date());
+        String message = (params != null && params.length > 0) ? String.format(s, params) : s;
+        System.out.printf("[%s] [%s] %s:%n", time, logLevel, message);
     }
 }
